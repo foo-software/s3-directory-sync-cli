43bc992b106f611fab5fcf2d283f30866ceeb6d7
Adam Henson <adamhenson1979@gmail.com>
2019-07-20 23:18:39 -0400

    Prettier.

--- src/helpers/arguments.js
+++ src/helpers/arguments.js
@@ -1,5 +1,5 @@
 // if the value is assigned as an argument get that... otherwise `undefined`
-const getArgument = (name) => {
+const getArgument = name => {
   const index = process.argv.indexOf(`--${name}`);
 
   if (index < 0) {
@@ -9,15 +9,14 @@ const getArgument = (name) => {
   return process.argv[index + 1];
 };
 
-export const convertOptionsFromArguments = options => Object.keys(options)
-  .reduce((accumulator, current) => {
+export const convertOptionsFromArguments = options =>
+  Object.keys(options).reduce((accumulator, current) => {
     // get the argument value
     const argumentValue = getArgument(current);
 
     // if the value doesn't exist from an argument, use the existing option / value
-    let value = typeof argumentValue !== 'undefined'
-      ? argumentValue
-      : options[current];
+    let value =
+      typeof argumentValue !== 'undefined' ? argumentValue : options[current];
 
     // convert string boolean to boolean
     if (typeof value === 'string') {
@@ -31,6 +30,6 @@ export const convertOptionsFromArguments = options => Object.keys(options)
 
     return {
       ...accumulator,
-      [current]: value,
+      [current]: value
     };
   }, {});

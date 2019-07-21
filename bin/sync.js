b1d923f92ffcdec5507e194c5110ea4607845d77
Adam Henson <adamhenson1979@gmail.com>
2019-07-20 22:49:41 -0400

    cli.

--- bin/sync.js
+++ bin/sync.js
@@ -1,7 +1,12 @@
 #! /usr/bin/env node
 const s3 = require('s3');
+const ProgressBar = require('cli-progress-bar');
+const package = require('../package.json');
 const { convertOptionsFromArguments } = require('../helpers/arguments');
 
+console.log(`${package.name}: sync starting...`);
+
+// default options from environment variables
 const defaultOptions = {
   S3_DIRECTORY_SYNC_ACCESS_KEY_ID: process.env.S3_DIRECTORY_SYNC_ACCESS_KEY_ID,
   S3_DIRECTORY_SYNC_ACL: process.env.S3_DIRECTORY_SYNC_ACL || 'public-read',
@@ -12,8 +17,10 @@ const defaultOptions = {
   S3_DIRECTORY_SYNC_STRICT: process.env.S3_DIRECTORY_SYNC_STRICT || false,
 };
 
+// override options with any that are passed in as arguments
 const options = convertOptionsFromArguments(defaultOptions);
 
+// create the AWS S3 client
 const client = s3.createClient({
   s3Options: {
     accessKeyId: options.S3_DIRECTORY_SYNC_ACCESS_KEY_ID,
@@ -21,7 +28,7 @@ const client = s3.createClient({
   },
 });
 
-const params = {
+const uploader = client.uploadDir({
   localDir: options.S3_DIRECTORY_SYNC_LOCAL_DIRECTORY,
   deleteRemoved: options.S3_DIRECTORY_SYNC_STRICT,
   s3Params: {
@@ -29,18 +36,25 @@ const params = {
     Bucket: options.S3_DIRECTORY_SYNC_BUCKET,
     Prefix: options.S3_DIRECTORY_SYNC_REMOTE_DIRECTORY,
   },
-};
-
-const uploader = client.uploadDir(params);
-
-uploader.on('error', function(err) {
-  console.error("unable to sync:", err.stack);
 });
 
-uploader.on('progress', function() {
-  console.log("progress", uploader.progressAmount, uploader.progressTotal);
+uploader.on('error', (error) => {
+  throw Error(error);
 });
 
-uploader.on('end', function() {
-  console.log("done uploading");
+// display a progress bar
+const bar = new ProgressBar();
+
+uploader.on('progress', () => {
+  const percentCompleted = uploader.progressAmount / uploader.progressTotal;
+  if (typeof percentCompleted === 'number') {
+    bar.show('current', percentCompleted);
+    bar.pulse('total');
+  }
+});
+
+uploader.on('end', () => {
+  bar.hide();
+  console.log(`${package.name}: sync completed ☀️ 🌴`);
+  console.log(`${package.name}: brought to you by Foo (https://www.foo.software)`);
 });
